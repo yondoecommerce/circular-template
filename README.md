72759ccd9f71a983f0177eb3da9b90db90a0f53b
james kim <james@yondo.com>
2016-08-17 09:24:06 +1000

    Added Circular template

---
+++ README.md
@@ -0,0 +1,16 @@
+Circular Template
+================
+This is a copy of the *circular [Yondo](http://www.yondo.com) Template*. It can be used as a sample for your own template development.
+
+## What is Yondo? ##
+Yondo is a web based platform providing video e-commerce services for companies and organizations. It provides online video subscriptions, pay-per-view rentals & live video sessions. 
+
+## What is an Yondo Template? ##
+Included with an Yondo account is a *hosted store website*. This provides a front-end for your customers to use when browsing your live session listings, and video-on-demand products. A highly customizable template engine provides the web content for this front-end. Every Yondo *store* uses either a standard template, or a custom installed template. **A template is basically a collection of html, css and other standard files you would find on a website.**
+
+## How do I develop a Template? ##
+Go to the [Yondo Partner Network](http://partners.yondo.com/signup) and sign up for a developer account. Here you can create and upload your template, and create a development store to test your template. The editing functions are quite basic, most developers create their template externally and just upload when done.
+
+###My template is empty, how do I get started?###
+[Download a Zip](https://github.com/expertory/circular-template/archive/master.zip) of this repository and upload it to the *files* section of your template. This will load the template with the files from this sample template.
+
---
+++ assets/scripts/portfolio.js
@@ -0,0 +1,74 @@
+﻿var PortfolioTemplates = {
+    VideoTemplate: function (assetInfo) {
+        var result = "";
+
+        if (assetInfo.Error)
+            result = 
+                "<a href='" + assetInfo.VideoUrl + "'>" +
+                    "<div class='image-container'>" +
+                        "<img style='width: 100px;height: 100px;'/>" +
+                        "<div>" + assetInfo.Error + "</div>" +
+                        "<div class='play-button' style='top:40% !important'>" +
+                            "<i class='fa fa-play fa-2x'></i>" +
+                        "</div>" +
+                    "</div>" +
+                "</a>" +
+                "<div>" + assetInfo.Title + "</div>";
+        else
+            result = 
+                "<a href='" + assetInfo.VideoUrl + "'>" +
+                    "<div class='image-container'>" +
+                        "<img src='" + assetInfo.ThumbSrc + "' />" +
+                        "<div class='play-button'>" +
+                            "<i class='fa fa-play fa-2x'></i>" +
+                        "</div>" +
+                    "</div>" +
+                "</a>" +
+                "<div>" + assetInfo.Title + "</div>";
+
+
+        return result;
+    }
+};
+
+var Contains = function(it) { return this.indexOf(it) != -1; };
+
+$('.video-preview.loading').each(function() {
+    var endpoint = "/api/OEmbed" + '?url=' + encodeURIComponent($(this).data('videourl'));
+    var element = this;
+    $.ajax({
+        url: endpoint,
+        dataType: 'json',
+        success: function (data) {
+            var assetInfo = {
+                VideoUrl: $(element).data('videourl'),
+                Description: data.description,
+                ThumbSrc: data.thumbnail_url,
+                Html: data.html,
+                Title: data.title
+            };
+            
+            result = PortfolioTemplates.VideoTemplate(assetInfo);
+            $(element).html(result);
+        },
+        error: function (data) {
+            var assetInfo = {
+                VideoUrl: $(element).data('videourl'),
+                Error: "Unable to load preview"
+            };
+            
+            result = PortfolioTemplates.VideoTemplate(assetInfo);
+            $(element).html(result);
+        },
+        complete: function (data) {
+            $('.video-preview > a').magnificPopup({
+                type: 'iframe'
+            });
+        }
+
+    });
+});
+
+$('.image-preview > a').magnificPopup({
+    type: 'image' 
+});
